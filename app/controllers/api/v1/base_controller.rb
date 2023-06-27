347ee1c06f5c161bef9aa18a1512aee47573ed97
juliatruji <trujillomjulia@gmail.com>
2023-06-26 23:42:34 -0500

    add  config cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::BaseController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from Pundit::NotAuthorizedError, with: :not_found
   protect_from_forgery with: :null_session
+  before_action :set_cors_headers
   skip_before_action :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   layout false
 
@@ -32,4 +33,19 @@ class Api::V1::BaseController < ActionController::Base
   def render_not_found
     render json: { status: "error", message: "Resource not found" }, status: :not_found
   end
+
+  def set_cors_headers
+    response.set_header('Access-Control-Allow-Origin', origin)
+    response.set_header('Referrer-Policy', 'strict-origin-when-cross-origin')
+    response.set_header('X-Content-Type-Options', 'nosniff')
+    response.set_header('X-Frame-Options', 'SAMEORIGIN')
+    response.set_header('X-XSS-Protection', '1; mode=block')
+    # response.set_header('Content-Security-Policy', "default-src 'self' https:; " \
+    #     "img-src 'self' https:" \
+    #     "media-src 'none'; " \
+    #     "object-src 'none'; " \
+    #     "script-src 'self'; " \
+    #     "style-src 'self' ")
+  end
+  
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,27 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    # allow do
+    #   origins 'http://192.168.0.131'
+  
+    #   resource '*',
+    #     headers: :any,
+    #     methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Per-Page', 'Total']
+    # end
+    # allow do
+    #   origins 'http://192.168.0.131'
+    #   resource '/public/*', headers: :any, methods: :get
+  
+    #   # Only allow a request for a specific host
+    #   resource '/api/v1/*',
+    #       headers: :any,
+    #       methods: :get,
+    #       if: proc { |env| env['HTTP_HOST'] == '192.168.0.131' }
+    # end
+  end
+  # Rails.application.config.hosts << "localhost"
+  # Rails.application.config.hosts << "192.168.0.131"
+  
+  Rails.application.config.hosts = ["192.168.0.131", "localhost", "go.digitalcerti.com"]
+  
