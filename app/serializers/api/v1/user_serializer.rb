1ef580dbaade3eb9ef237bbaeb18e8686653aa45
juliatruji <trujillomjulia@gmail.com>
2023-07-05 22:08:54 -0500

    add location to user_serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -9,4 +9,5 @@ class Api::V1::UserSerializer < Api::V1::ApplicationSerializer
              :created_at,
              :updated_at
   belongs_to :client, serializer: Api::V1::ClientSerializer
+  has_many :locations, through: :user_locations, serializer: Api::V1::LocationSerializer
 end
