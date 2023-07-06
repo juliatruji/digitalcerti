f2bd417c26cac50a74d1da88a0c2e9127774f32b
juliatruji <trujillomjulia@gmail.com>
2023-07-05 23:23:45 -0500

    fix

--- spec/swagger/geolocation_schema.rb
+++ spec/swagger/geolocation_schema.rb
@@ -3,12 +3,12 @@ module GeolocationSchema
     type: 'object',
     properties: {
       id: { type: :integer, description: 'ID' },
-      country_id: { type: :integer, description: 'ID pais' },
-      department_id: { type: :integer, description: 'ID Departamento' },
-      province_id: { type: :integer, description: 'ID Provincia' },
-      district_id: { type: :integer, description: 'ID Distrito' },
       created_at: { type: :string, format: 'date-time', description: 'Fecha de creación' },
-      updated_at: { type: :string, format: 'date-time', description: 'Fecha de actualización' }
+      updated_at: { type: :string, format: 'date-time', description: 'Fecha de actualización' },
+      country: { '$ref': '#/components/schemas/country' },
+      department: { '$ref': '#/components/schemas/department' },
+      province: { '$ref': '#/components/schemas/province' },
+      district: { '$ref': '#/components/schemas/district' }
     },
     required: [:id]
   }.freeze
