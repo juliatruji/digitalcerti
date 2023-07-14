1922c680d7c3a1d0fcad023f6d2f3b1856ea53ff
juliatruji <trujillomjulia@gmail.com>
2023-07-14 01:16:56 -0500

    fix

--- app/serializers/api/v1/client_serializer.rb
+++ app/serializers/api/v1/client_serializer.rb
@@ -15,9 +15,15 @@ class Api::V1::ClientSerializer < Api::V1::ApplicationSerializer
              :colors,
              :domain,
              :banner,
+             :geolocation,
              :created_at,
              :updated_at
-  belongs_to :geolocation, serializer: Api::V1::GeolocationSerializer
+  # belongs_to :geolocation, serializer: Api::V1::GeolocationSerializer
+  def geolocation
+    return nil if object.geolocation.nil?
+
+    ActiveModel::SerializableResource.new(object.geolocation, serializer: Api::V1::GeolocationSerializer)
+  end
   attribute(:banner) { file_serializer(:banner) }
   attribute(:logo) { file_serializer(:logo) }
 end
